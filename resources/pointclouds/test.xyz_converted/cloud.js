abefeb2331de00208a16d4005a34ebcb6bb9128e
Generacy <generacy@gmail.com>
2014-06-30 16:55:03 -0400

    got external pointcloud working

---
+++ resources/pointclouds/test.xyz_converted/cloud.js
@@ -0,0 +1,20 @@
+{
+	"version": "1.1",
+	"octreeDir": "data",
+	"boundingBox": {
+		"lx": 0,
+		"ly": 0,
+		"lz": 0,
+		"ux": 1,
+		"uy": 1,
+		"uz": 1
+	},
+	"pointAttributes": [
+		"POSITION_CARTESIAN",
+		"COLOR_PACKED"
+	],
+	"spacing": 1,
+	"hierarchy": [
+		["r",4]
+	]
+}
